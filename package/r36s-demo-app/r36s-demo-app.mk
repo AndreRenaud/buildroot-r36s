675524b3a0ed3025672e1d9bbfa1665815429eda
Andre Renaud <andre@ignavus.net>
2024-06-21 10:02:51 +1200

    r36s-demo-app: Fix up SDL dependencies

--- package/r36s-demo-app/r36s-demo-app.mk
+++ package/r36s-demo-app/r36s-demo-app.mk
@@ -7,4 +7,6 @@
 R36S_DEMO_APP_VERSION = main
 R36S_DEMO_APP_SITE = $(call github,AndreRenaud,r36s-demo-app,$(R36S_DEMO_APP_VERSION))
 
+R36S_DEMO_APP_DEPENDENCIES += sdl2_mixer sdl2_ttf sdl2_gfx
+
 $(eval $(golang-package))
